bc4cfee28779ae76af9a4c8625949a2ce1dfd65e
downloard <henrytheb@gmx.de>
2011-04-06 18:48:18 +0200

    Add FileHelpterTests to test suite

--- unittests/src/unittests/UyooTestSuite.java
+++ unittests/src/unittests/UyooTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import unittests.core.FileHelperTests;
 import unittests.core.FileWatcherTest;
 import unittests.data.GroupedLogLineTests;
 import unittests.data.LogFileListenerTest;
@@ -21,6 +22,7 @@ import unittests.setup.UyooSettingsTests;
 	           LogFileTests.class,
 	           LogFilterFileTest.class,
 	           FileWatcherTest.class,
+	           FileHelperTests.class,
 	           UyooSettingsTests.class
 	          })
 public class UyooTestSuite {
